94b767b8798dbad37080f454b83246636ec39f7f
Hoàng Văn Hòa <45493195+hoangvanhoa1701@users.noreply.github.com>
2019-12-13 14:31:57 +0700

    update learn reactjs easy

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,14 @@ export default class App extends React.Component {
         <h2 className="BorderTop">Basic Calculator</h2>
         <Calculator />
 
-        <h2 className="BorderTop">Click change</h2>
+        <h2 className="BorderTop">Click Change</h2>
         <ClickChange />
+
+        <h2 className="BorderTop">State and Props Example</h2>
+        <StatefulExample />
+
+        {/* <h2 className="BorderTop">Props Validation</h2>
+        <PropsValidation /> */}
       </div>
     );
   }
@@ -83,5 +89,105 @@ class Calculator extends React.Component {
     );
   }
 }
+
+class StatefulExample extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data:
+        [
+          {
+            "id": 1,
+            "name": "Foo",
+            "age": "20"
+          },
+          {
+            "id": 2,
+            "name": "Bar",
+            "age": "30"
+          },
+          {
+            "id": 3,
+            "name": "Baz",
+            "age": "40"
+          }
+        ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <HeaderStatefulExample />
+        <table>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow personProp={person} key={i}></TableRow>)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class HeaderStatefulExample extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Stateful Header</h3>
+      </div>
+    );
+  }
+}
+
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.personProp.id}</td>
+        <td>{this.props.personProp.name}</td>
+        <td>{this.props.personProp.age}</td>
+      </tr>
+    );
+  }
+}
+
+// class PropsValidation extends React.Component {
+//   render() {
+//      return (
+//         <div>
+//            <h3>Array: {this.props.propArray}</h3>
+//            <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
+//            <h3>Func: {this.props.propFunc(3)}</h3>
+//            <h3>Number: {this.props.propNumber}</h3>
+//            <h3>String: {this.props.propString}</h3>
+//            <h3>Object: {this.props.propObject.objectName1}</h3>
+//            <h3>Object: {this.props.propObject.objectName2}</h3>
+//            <h3>Object: {this.props.propObject.objectName3}</h3>
+//         </div>
+//      );
+//   }
+// }
+
+// PropsValidation.propTypes = {
+//   propArray: React.PropTypes.array.isRequired,
+//   propBool: React.PropTypes.bool.isRequired,
+//   propFunc: React.PropTypes.func,
+//   propNumber: React.PropTypes.number,
+//   propString: React.PropTypes.string,
+//   propObject: React.PropTypes.object
+// }
+
+// PropsValidation.defaultProps = {
+//   propArray: [1,2,3,4,5],
+//   propBool: true,
+//   propFunc: function(e){return e},
+//   propNumber: 1,
+//   propString: "String value...",
+  
+//   propObject: {
+//      objectName1:"objectValue1",
+//      objectName2: "objectValue2",
+//      objectName3: "objectValue3"
+//   }
+// }
 
 // export default App;
